d8007fa42f493dc20fb305f823c0844582e942d0
Ananth Suryanarayana <anantha@juniper.net>
2016-02-17 16:44:13 -0800

    Clear() autogen structures in constructor
    When autogen structures are carved off stack, the member variables remain
    uninitialized. Instead of explicitly clearing them by calling Clear(), make
    generateds create a contructor which calls Clear() automatically
    
    Change-Id: I982869541c984b2f7650e06e0b1044ee4c33a41d
    Closes-bug: #1546813

--- type_classgen.py
+++ type_classgen.py
@@ -17,6 +17,7 @@ class TypeClassGenerator(object):
 
         file.write('\nstruct %s : public AutogenProperty {\n' % ctype.getName())
 
+        file.write('    %s();\n' % ctype.getName())
         file.write('    virtual ~%s();\n' % ctype.getName())
 
         members = ctype.getDataMembers()
@@ -83,6 +84,14 @@ class TypeImplGenerator(object):
         pass
 
     def GenerateType(self, file, ctype):
+        construct = """
+%(class)s::%(class)s() {
+    Clear();
+}
+""" % {'class': ctype.getName()}
+
+        file.write(construct)
+
         destruct = """
 %(class)s::~%(class)s() {
 }
