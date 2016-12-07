76e64fd3204aff433ad2383258e65e62809ca41c
Prakash Bailkeri <prakashmb@juniper.net>
2016-12-07 22:26:48 +0530

    Support attribute for xmpp messaging
    Currently, XML for xmpp message doesn't support attribute declaration.
    
    Example:
    Mobility extended community needs two information. sequence number and sticky bit
    In the current schema, this needs to be defined as two different element.
            <xsd:element name="sequence-number" type="xsd:integer"/>
            <xsd:element name="sticky" type="xsd:integer"/>
    
    This will add two xml nodes to xmpp message making it more verbose and bulky
    <xmpp-message>
        <sequence-number> 1 </sequence-number>
        <sticky> False </sticky>
    </xmpp-message>
    
    This xmpp message can be represented with one XML node with attributes.
    <xmpp-message>
        <mobility seqno="2" sticky="true" />
    </xmpp-message>
    
    Add support for XML attribute in schema and auto generation of types/parser code
    
    Change-Id: Idbd238ec414f7b18c897c8482cfdff04c0f77876
    Related-bug: #1645092

--- type_model.py
+++ type_model.py
@@ -27,6 +27,7 @@ class MemberInfo(object):
 class ComplexType(object):
     def __init__(self, name):
         self._name = name
+        self._is_attribute = False
         self._data_types = []
         self._data_members = []
 
@@ -48,37 +49,58 @@ class ComplexType(object):
         self._xsd_type = xsdTypeDict[self._name]
 
         children = self._xsd_type.getChildren()
-        for child in children:
-            if child.isComplex():
-                descendent = ComplexTypeLocate(xsdTypeDict, cTypeDict, child.getType())
-                self._data_types.append(descendent)
-                cpptype = child.getType()
-                jtype = child.getType()
-                cppname = child.getCleanName()
-            else:
-                cpptype = getCppType(child.getType())
-                jtype = getJavaType(child.getType())
+        if children:
+            for child in children:
+                if child.isComplex():
+                    descendent = ComplexTypeLocate(xsdTypeDict, cTypeDict, child.getType())
+                    self._data_types.append(descendent)
+                    cpptype = child.getType()
+                    jtype = child.getType()
+                    cppname = child.getCleanName()
+                else:
+                    cpptype = getCppType(child.getType())
+                    jtype = getJavaType(child.getType())
+                    if cpptype == 'void':
+                        logger = logging.getLogger('type_model')
+                        logger.warning('simpleType %s: unknown' % child.getType())
+                    cppname = child.getCleanName()
+
+                member = MemberInfo()
+                member.elementname = child.getName()
+                member.membername = CppVariableName(cppname)
+                member.xsd_object = child
+                member.isComplex = child.isComplex()
+                if child.getMaxOccurs() > 1:
+                    member.membername = cppname# + '_list'
+                    member.sequenceType = cpptype
+                    cpptype = 'std::vector<%s>' % cpptype
+                    jtype = 'List<%s>' % jtype
+                    member.isSequence = True
+
+                member.ctypename = cpptype
+                member.jtypename = jtype
+                member.default = child.getDefault()
+                self._data_members.append(member)
+        else:
+            attributes = self._xsd_type.getAttributeDefs().values()
+            if attributes:
+                self._is_attribute = True
+            for attribute in attributes:
+                cpptype = getCppType(attribute.getType())
+                jtype = getJavaType(attribute.getType())
                 if cpptype == 'void':
                     logger = logging.getLogger('type_model')
-                    logger.warning('simpleType %s: unknown' % child.getType())
-                cppname = child.getCleanName()
-
-            member = MemberInfo()
-            member.elementname = child.getName()
-            member.membername = CppVariableName(cppname)
-            member.xsd_object = child
-            member.isComplex = child.isComplex()
-            if child.getMaxOccurs() > 1:
-                member.membername = cppname# + '_list'
-                member.sequenceType = cpptype
-                cpptype = 'std::vector<%s>' % cpptype
-                jtype = 'List<%s>' % jtype
-                member.isSequence = True
-
-            member.ctypename = cpptype
-            member.jtypename = jtype
-            member.default = child.getDefault()
-            self._data_members.append(member)
+                    logger.warning('simpleType %s: unknown' % attribute.getType())
+                cppname = attribute.getCleanName()
+                member = MemberInfo()
+                member.elementname = attribute.getName()
+                member.membername = CppVariableName(cppname)
+                member.xsd_object = attribute
+                member.isComplex = False
+                member.ctypename = cpptype
+                member.jtypename = jtype
+                member.default = attribute.getDefault()
+                self._data_members.append(member)
 
 
 def ComplexTypeLocate(xsdTypeDict, cTypeDict, xtypename):
